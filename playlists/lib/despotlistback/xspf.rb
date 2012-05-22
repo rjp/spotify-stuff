e1e8a25e3217c8466c2d0dbaca1b17c0fdabfc83
rjp <zimpenfish@gmail.com>
2012-05-22 23:16:11 +0100

    Changes to support using the libdespotify ruby bindings instead of despotify-gateway Preliminary work, incomplete, pending proper review of this and the despotify-ruby gem

--- playlists/lib/despotlistback/xspf.rb
+++ playlists/lib/despotlistback/xspf.rb
@@ -1,10 +1,21 @@
-require 'fileutils'
+# Output our collection of tracks as an XSPF-compliant playlist. 
+#
+# [xspf]: http://xspf.org/xspf-v1.html 
+# [notes]: http://wiki.xiph.org/XSPF_v1_Notes_and_Errata
+# [xspfgem]: http://xspf.rubyforge.org/
+# 
+# Because we want to use multiple <meta> and <identifier> elements, we can't use the [xspf gem][xspfgem].
+# In lieu of a proper libxspf based alternative, we use Nokogiri::XML::Builder to construct our XML output.
 
 require 'rubygems'
 require 'nokogiri'
+require 'fileutils'
+
+# We'll be adding "proper" Spotify links for track, user, artist, album which means we need to be able to
+# convert the 16 byte API IDs into the Base 62 encoded IDs.
 require 'despotlistback/convert'
 
-# http://wiki.xiph.org/XSPF_v1_Notes_and_Errata#Version_information_in_key_attributes
+# All our <meta> elements are versioned - see the [xspf notes][notes].
 MetaBase = "http://frottage.org/xspf/spotify/"
 MetaVer = "/1/0"
 
@@ -12,71 +23,70 @@ def metauri(key)
     return MetaBase + key + MetaVer
 end
 
+# "Naming is hard". 
 class DespoGClient
     def write_playlist(playlist)
+        # Technically this is redundant since we do this later on. FIXME
         begin
             FileUtils.mkdir_p(@outputdir)
         rescue
         end
 
-        pluri = id2uri(playlist[:pid][0..31])
+        pluri = id2uri(playlist.id[0..31])
+
         pl = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
             xml.playlist('version' => '1', 'xmlns' => 'http://xspf.org/ns/0/') {
-                xml.title playlist[:name].to_s
+                xml.title playlist.name.to_s
                 xml.creator "Spotify/#{username}" # spotify link?
-                xml.info "http://open.spotify.com/user/#{playlist[:user]}"
-                xml.location "http://open.spotify.com/user/#{playlist[:user]}/playlist/#{pluri}"
-                xml.identifier "http://open.spotify.com/user/#{playlist[:user]}/playlist/#{pluri}"
+                xml.info "http://open.spotify.com/user/#{playlist.author}"
+                xml.location "http://open.spotify.com/user/#{playlist.author}/playlist/#{pluri}"
+                xml.identifier "http://open.spotify.com/user/#{playlist.author}/playlist/#{pluri}"
                 xml.meta Time.now.to_i, :rel => metauri("epoch")
                 xml.meta "despotway.rb", :rel => metauri("creator")
                 xml.trackList {
-                    playlist[:tracks].each do |track|
-                        # TODO refactor this out into a sanitising function
-                        if track[:uri] =~ /^spotify/ then
-                            track[:to_link] = track[:uri]
-                        else
-                            if track[:uri].nil? then # shouldn't happen because we protect against this in load_track
-                                $stderr.puts "!! #{track.inspect} has no URI in output"
-                                track[:uri] = "_daddy_"
-                            end
-                            track[:to_link] = "http://open.spotify.com/track/" + track[:uri]
-                        end
+                    playlist.tracks.each do |track|
+                        uri = id2uri(track.id)
+                        to_link = "http://open.spotify.com/track/" + uri
+                        artist = track.artists.first # meh
+
                         xml.track {
-                            xml.location track[:to_link]
-                            xml.title track[:title].to_s
-                            xml.creator track[:artist].to_s
-                            xml.album track[:album].to_s
-                            xml.duration track[:duration].to_s
+                            xml.location to_link
+                            xml.location track.to_uri
+                            xml.title track.title.to_s
+                            xml.creator artist.name.to_s
+                            xml.album track.album.to_s
+                            xml.duration track.length.to_s
 
-                            # we don't always have an ISRC code
-                            if not track[:isrc].nil? then
-                                  xml.identifier "isrc:" + track[:isrc]
-                            end
-
-                            # do we have any album metadata to add?
-                            if not track[:album_meta].nil? then
-                                # we always have a :id subhash by design tho' it may be empty
-                                track[:album_meta][:id].keys.each do |id_type|
-                                    val = track[:album_meta][:id][id_type]
-                                    # anything in the [:id] hash gets added as that type of identifier 
-                                    if not val.nil? then
-                                        xml.identifier "#{id_type}:#{val}"
-                                    end
-                                end
-                            end
+                            $stderr.puts track.metadata.inspect
+#                            # we don't always have an ISRC code
+#                            if not track[:isrc].nil? then
+#                                  xml.identifier "isrc:" + track[:isrc]
+#                            end
+#
+#                            # do we have any album metadata to add?
+#                            if not track[:album_meta].nil? then
+#                                # we always have a :id subhash by design tho' it may be empty
+#                                track[:album_meta][:id].keys.each do |id_type|
+#                                    val = track[:album_meta][:id][id_type]
+#                                    # anything in the [:id] hash gets added as that type of identifier 
+#                                    if not val.nil? then
+#                                        xml.identifier "#{id_type}:#{val}"
+#                                    end
+#                                end
+#                            end
 
                             # we don't always have a track number
-                            if not track[:index].nil? then
-                                  xml.trackNum track[:index].to_s
+                            if not track.tracknumber.nil? then
+                                  xml.trackNum track.tracknumber.to_s
                             end
 
                             # if we have :arid, this is a real Spotify track, add extra links for niceness
-                            if not track[:arid].nil? then
-                                xml.meta track[:arid], :rel => metauri("artist-id")
-                                xml.meta track[:aid], :rel => metauri("album-id")
-                                xml.meta track[:tid], :rel => metauri("track-id")
-                                l_artist = id2uri(track[:arid])
-                                l_album = id2uri(track[:aid])
+                            if not artist.id.nil? then
+                                xml.meta artist.id, :rel => metauri("artist-id")
+                                xml.meta track.album_id, :rel => metauri("album-id")
+                                xml.meta track.id, :rel => metauri("track-id")
+                                l_artist = id2uri(artist.id)
+                                l_album = id2uri(track.album_id)
                                 xml.meta "http://open.spotify.com/artist/#{l_artist}", :rel => metauri("artist")
                                 xml.meta "http://open.spotify.com/album/#{l_album}", :rel => metauri("album")
                             end
@@ -86,15 +96,15 @@ class DespoGClient
             }
         end
 
-        safename = playlist[:name].gsub(/[^0-9a-zA-Z]/, "_")
+        safename = playlist.name.gsub(/[^0-9a-zA-Z]/, "_")
 
         replace = {
             "%u" => @username,
-            "%c" => playlist[:user],
-            "%w" => [@username, playlist[:user]].uniq.join(':'),
-            "%i" => playlist[:pid],
-            "%s" => playlist[:tracks].size.to_s,
-            "%d" => playlist[:user] == @username ? "" : "subscribed/",
+            "%c" => playlist.author,
+            "%w" => [@username, playlist.author].uniq.join(':'),
+            "%i" => playlist.id,
+            "%s" => playlist.tracks.size.to_s,
+            "%d" => playlist.author == @username ? "" : "subscribed/",
             "%n" => safename
         }
 
